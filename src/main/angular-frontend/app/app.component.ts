7fb68a7de72d41606c5ec9e931cc4176a45bbd08
Rebekah McVey <rebekah.mcvey@e-gineering.com>
2016-11-10 09:11:42 -0500

    Move app component template to separate html

--- src/main/angular-frontend/app/app.component.ts
+++ src/main/angular-frontend/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Recipe App</h1>`
+    templateUrl: `/app/app.component.html`
 })
 export class AppComponent { }
